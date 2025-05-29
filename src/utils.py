cf3262fd514aea80cd62be08dbba272e2897f5e8
Kevin Bai <kbai34@gatech.edu>
2025-05-29 18:40:46 -0400

    app update

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 from sklearn.model_selection import GridSearchCV
@@ -22,7 +23,7 @@ def save_object(file_path, obj):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
     
